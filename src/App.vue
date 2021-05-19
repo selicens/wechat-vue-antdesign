7dccb1905c7995538fc67437a889f24b607e7b50
luoyf <luoyf@huaqiweb.com>
2021-05-19 20:27:50 +0800

    Element改为AntDesign

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
+    <About/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import About from "./views/About"
+  export default {
+    name: 'App',
+    components: {About}
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html,body,#app{
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 Vue.config.productionTip = false
+Vue.use(Antd)
+Vue.prototype.$axios = axios;
+router.beforeEach((to,from,next) =>{
+  if(to.meta.title){
+    document.title = to.meta.title
+  }
+  next();
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
